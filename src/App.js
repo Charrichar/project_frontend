ad243ea6f36ccb1782ab4191ad20c52e71047fe5
jeffbat <75343055+jeffbat@users.noreply.github.com>
2021-08-08 17:18:30 -0400

    fix compile bug

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 
     useEffect(()=>{
         axios
-            .get('https://group-project-jeff-dwayne.herokuapp.com/news');
+            .get('https://group-project-jeff-dwayne.herokuapp.com/news')
             .then((response)=>{
                 setNewArticleList(response.data);
             })
