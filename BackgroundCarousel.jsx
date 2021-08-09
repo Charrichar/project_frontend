87a895ad2d7dc423f31b3df4560fe8facf7639e6
Oduaine Miller <oduainemiller@Oduaines-Air.fios-router.home>
2021-08-09 11:12:56 -0400

    made some minor changes

--- BackgroundCarousel.jsx
+++ BackgroundCarousel.jsx
@@ -1,3 +1,10 @@
+///////////needs to go in app.js/////////////
+import {BackgroundCarousel} from "./BackgroundCarousel"
+///////////needs to go in app.js/////////////
+
+
+
+
 import * as React from 'react'
 import {StyleSheet, View, ScrollView, Dimensions, Image} from 'react-native'
 
@@ -35,3 +42,11 @@ class BackgroundCarousel extends React.Component {
 }
 
 export {BackgroundCarousel}
+
+
+const images = [
+  "https://images.unsplash.com/photo-1490814525860-594e82bfd34a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmxhY2slMjBhbmQlMjB3aGl0ZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1529432337323-223e988a90fb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmxhY2slMjBhbmQlMjB3aGl0ZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1549281899-f75600a24107?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGJsYWNrJTIwYW5kJTIwd2hpdGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1557428894-56bcc97113fe?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fG5ld3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+]
